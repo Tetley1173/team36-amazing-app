910c84bd0477a5b62c3fa0d04155c10d82a261f8
Shannon Tetley <n6255078@qut.edu.au>
2022-04-09 18:10:55 +1000

    Implemented a class system for separating code that renders the UI. For example the AssetsTab class extends the UserInterface class to define the buttons that are located in the Assets tab. The UserInterface class calls the assestsContents method to render what's in that tab.

---
+++ src/gui/AssetsTab.java
@@ -0,0 +1,14 @@
+package gui;
+
+import javax.swing.*;
+
+public class AssetsTab extends UserInterface {
+
+    public static void assetContents(JPanel p) {
+        p.add(new JButton("Select Entry Image"));
+        p.add(new JButton("Select Exit Image"));
+        p.add(new JButton("Select Logo 1 Image"));
+        p.add(new JButton("Select Logo 2 Image"));
+    }
+
+}
--- src/gui/UserInterface.java
+++ src/gui/UserInterface.java
@@ -1,19 +1,18 @@
 package gui;
 import javax.swing.*;
 import java.awt.*;
+import static gui.AssetsTab.assetContents;
 
 public class UserInterface {
 
     public static void mainInterface()
     {
         // Setting up canvas
-
         JFrame jframe = new JFrame("Maze Application");
         jframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         jframe.setPreferredSize(new Dimension(600,600));
 
         // Adding Menu option (Files)
-
         JMenuBar menuBar = new JMenuBar();
 
         JMenu FileMenu = new JMenu("File");
@@ -30,18 +29,21 @@ public class UserInterface {
         menuBar.add(editMenu);
         jframe.setJMenuBar(menuBar);
 
+        // Edit tab
         // Buttons "Toggle OP" & "Generate Maze"
-        JPanel buttonpanel = new JPanel();
-        buttonpanel.add(new JButton("Toggle OP"));
-        buttonpanel.add(new JButton("Generate Maze"));
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(new JButton("Toggle OP"));
+        buttonPanel.add(new JButton("Generate Maze"));
 
         JTabbedPane panel = new JTabbedPane();
         //panel.setBorder(BorderFactory.createTitledBorder("Label Names"));
 
         // Assets Tab
-        JPanel interiorPanel = new JPanel();
-        panel.add("Assets", interiorPanel);
-        panel.add("Edit", buttonpanel);
+        JPanel assetsPanel = new JPanel();
+        assetContents(assetsPanel);
+
+        panel.add("Assets", assetsPanel);
+        panel.add("Edit", buttonPanel);
         panel.add("Export", new JLabel());
 
 
