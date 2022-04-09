97ab5583c70848d54d867fd9ef1406f23efaab36
Shannon Tetley <n6255078@qut.edu.au>
2022-04-09 16:11:51 +1000

    I added Dilshans Maze class and renamed it to the UserInterface class. We now have a basic user interface working.

---
+++ src/gui/UserInterface.java
@@ -0,0 +1,57 @@
+package gui;
+import javax.swing.*;
+import java.awt.*;
+
+public class UserInterface {
+
+    public static void main(String[] args)
+    {
+        // Setting up canvas
+
+        JFrame jframe = new JFrame("Maze Application");
+        jframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jframe.setPreferredSize(new Dimension(600,600));
+
+        // Adding Menu option (Files)
+
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu FileMenu = new JMenu("File");
+        FileMenu.add("New");
+        FileMenu.add("Load");
+        FileMenu.add("Exit");
+        menuBar.add(FileMenu);
+
+        // Adding Menu option (Edit)
+        JMenu editMenu = new JMenu("Edit");
+        editMenu.add("Cut");
+        editMenu.add("Copy");
+        editMenu.add("Paste");
+        menuBar.add(editMenu);
+        jframe.setJMenuBar(menuBar);
+
+        // Buttons "Toggle OP" & "Generate Maze"
+        JPanel buttonpanel = new JPanel();
+        buttonpanel.add(new JButton("Toggle OP"));
+        buttonpanel.add(new JButton("Generate Maze"));
+
+        JTabbedPane panel = new JTabbedPane();
+        //panel.setBorder(BorderFactory.createTitledBorder("Label Names"));
+        panel.add("Edit", buttonpanel);
+        panel.add("Export", new JLabel());
+
+        // Assets Tab
+
+        JPanel interiorPanel = new JPanel();
+        panel.add("Assets", interiorPanel);
+
+
+        // Finalising program
+
+        jframe.getContentPane().add(panel);
+        jframe.pack();
+        jframe.setLocationRelativeTo(null);
+        //jframe.setLocation(new Point(1000, 400));
+        jframe.setVisible(true);
+    }
+}
