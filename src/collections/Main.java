44cad6ea05112399aa1c67856aa77ac25112dca9
Shannon Tetley <n6255078@qut.edu.au>
2022-04-15 17:21:52 +1000

    Completed prototype of the AssetsTab class including documentation and error handling. The class now loads default images and places them next to each of the buttons.

--- src/collections/Main.java
+++ src/collections/Main.java
@@ -17,7 +17,6 @@ public class Main {
         //mainInterface();
         UserInterface frame = new UserInterface();
         frame.setVisible(true);
-        System.out.print("It does something!");
 
     }
 
