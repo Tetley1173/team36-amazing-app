e64eb7c29e011378e2bc8da64eba101cf415f96c
Georggiiee <Georgia.meszarossimon@connect.qut.edu.au>
2022-04-22 17:30:05 +1000

    Updated ExportTab Changes: - Added button options for exporting with a solution - Added button options for different exporting sizes

--- src/gui/ExportTab.java
+++ src/gui/ExportTab.java
@@ -1,15 +1,117 @@
 package gui;
 
 import javax.swing.*;
+import javax.swing.BorderFactory;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *  When I export a maze, I want to be able to have the option
+ *  of whether to export the maze with the solution or not:
+ *
+ *  When I export a maze, I want to be able to export multiple mazes
+ *  at once:
+ *
+ *  When I export a maze, I want it to be exported to a size relative
+ *  to the dimensions of the maze:
+ *
+ *  When I export a maze, I want control of where I can save the file.
+ */
 
 public class ExportTab extends JFrame{
-    private final JPanel temp;
-    private final JLabel label;
+    private final JPanel layOut;
+
+    // Title for tab
+    private final TitledBorder titlelabel;
+
+    // Radio buttons -> options for export features
+        // Export with solution or without
+    private JRadioButton solButton1;
+    private JRadioButton solButton2;
+
+        // Export pixel size
+    private JRadioButton sizeButton1;
+    private JRadioButton sizeButton2;
+
+        // Export as what file format - NOT FINISHED
+    private JRadioButton pdfButton;
+    private JRadioButton pngButton;
+
+    // Button for export
+    private JButton exportToButton;
+        // Dialog Button for export
+    private JDialog dialogWin;
+
     public ExportTab(JTabbedPane tabbedPane) {
-        label = new JLabel("This is the label for ExportTab");
-        temp = new JPanel(new BorderLayout());
-        temp.add(label, BorderLayout.SOUTH);
-        tabbedPane.add("Export",temp);
+        layOut = new JPanel(new GridLayout());
+        tabbedPane.add("Export",layOut);
+        titlelabel = BorderFactory.createTitledBorder("Export Configuration");
+        layOut.setBorder(titlelabel);
+
+        /**
+         *  Buttons:
+         */
+        solButton1 = new JRadioButton("With Solution");
+        solButton2 = new JRadioButton("Without Solution");
+        ButtonGroup g1 = new ButtonGroup(); // Group
+        g1.add(solButton1);
+        g1.add(solButton2);
+
+        sizeButton1 = new JRadioButton("800x800px");
+        sizeButton2 = new JRadioButton("400x400px");
+        ButtonGroup g2 = new ButtonGroup(); // Group
+        g2.add(sizeButton1);
+        g2.add(sizeButton2);
+
+        pdfButton = new JRadioButton("Export as PDF?");
+        pngButton = new JRadioButton("Export as PNG?");
+        ButtonGroup g3 = new ButtonGroup(); // Group
+        g3.add(pdfButton);
+        g3.add(pngButton);
+
+        exportToButton = new JButton("Export Maze?");
+        exportToButton.addActionListener(new ActionListener() {
+            @Override   // Open export dialog window
+            public void actionPerformed(ActionEvent e) {
+                dialogWin.setVisible(true);
+            }
+        });
+
+        /**
+         *  This section for pop up window after pressing export:
+         */
+        JFrame f = new JFrame();
+        dialogWin = new JDialog(f, "Dialog Example", true);
+        dialogWin.setLayout(new GridLayout(3,1,0,50));
+        dialogWin.add(new JLabel("Export Successful"));
+        JButton dialogExit = new JButton("OK");
+        dialogExit.setPreferredSize(new Dimension(150, 35));
+        dialogWin.add(dialogExit);
+        dialogWin.setSize(300,300);
+        dialogExit.addActionListener(new ActionListener() {
+            @Override   // Close export dialog window
+            public void actionPerformed(ActionEvent e) {
+                dialogWin.setVisible(false);
+            }
+        });
+
+        /**
+         *  Grid layout:
+         */
+        JPanel gridPanel = new JPanel(new GridLayout(0,1));
+        gridPanel.add(new JLabel("Export with solution?"));
+        gridPanel.add(solButton1);
+        gridPanel.add(solButton2);
+        gridPanel.add(new JLabel("Export size:"));
+        gridPanel.add(sizeButton1);
+        gridPanel.add(sizeButton2);
+        gridPanel.add(new JLabel("Export file:"));
+        gridPanel.add(pdfButton);
+        gridPanel.add(pngButton);
+
+        gridPanel.add(exportToButton);
+        layOut.add(gridPanel, BorderLayout.WEST);
     }
 }
