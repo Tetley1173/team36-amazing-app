cb394e98721f1be8cb5f3af1667ef26f9251f8dc
Shannon Tetley <n6255078@qut.edu.au>
2022-05-25 15:49:18 +1000

    Finished implementing the AssetsDataInterface. It is currently untested.

--- src/database/AssetsDataInterface.java
+++ src/database/AssetsDataInterface.java
@@ -9,52 +9,43 @@ import java.util.Set;
  * Adapted from the week 6 tutorial.
  * @author Shannon Tetley
  */
-public class AssetsDataInterface {
+public interface AssetsDataInterface {
 
     /**
-     * Adds a ImageAssetFile to the database, if they are not already in the database.
+     * Adds an ImageAssetFile to the database, if they are not already in the database.
      *
      * @param i ImageAssetFile to add
      */
-//    void addImageFile(ImageAssetFile i);
+    void addImageFile(ImageAssetFile i);
+
 
     /**
      * Extracts a ImageAssetFile from the database and creates an instance of the class, extraction is
      * done using a name to find it in the database.
      *
-     * @param name The name as a String to search for.
+     * @param name The name(string) to search for.
      * @return all details in a Person object for the name
      */
-//    ImageAssetFile getImageFile(String name);
+    ImageAssetFile getImageFile(String name);
 
     /**
      * Gets the number of addresses in the address book.
-     * Probably won't implement this one delete once confirmed ############################################
-     *
      * @return size of address book.
      */
-//    int getSize();
+    int getSize();
 
     /**
      * Deletes a Person from the address book.
-     * Make sure this is done someware in the system to remove unneeded assets ################################
+     * Make sure this is done somewhere in the system to remove unneeded assets ################################
      * Maybe manually by the user or conditionally when a maze is deleted.
      *
      * @param name The name to delete from the address book.
      */
-//    void deleteImageFile(String name);
+    void deleteAsset(String name);
 
     /**
      * Finalizes any resources used by the data source and ensures data is
      * persisited.
      */
-//    void close();
-
-    /**
-     * Retrieves a set of names from the data source that are used in
-     * the name list.
-     *
-     * @return set of names.
-     */
-//    Set<String> nameSet();
+    void close();
 }
--- src/database/ImageAssetDataSource.java
+++ src/database/ImageAssetDataSource.java
@@ -19,172 +19,156 @@ import static collections.HelperMethods.bufferedImageToBlob;
 /**
  * Class for retrieving data from the XML file holding the address list.
  */
-//public class ImageAssetDataSource implements AssetsDataInterface {
+public class ImageAssetDataSource implements AssetsDataInterface {
+
+    public static final String CREATE_TABLE =
+            "CREATE TABLE IF NOT EXISTS assetFiles ("
+                    + "idx INTEGER PRIMARY KEY /*!40101 AUTO_INCREMENT */ NOT NULL UNIQUE," // from https://stackoverflow.com/a/41028314
+                    + "name VARCHAR(30) NOT NULL UNIQUE,"
+                    + "imageFile BLOB(MAX),";
+
+    private static final String INSERT_ASSET = "INSERT INTO assetFiles (name, imageFile) VALUES (?, ?, ?, ?, ?);";
+
+    private static final String GET_NAMES = "SELECT name FROM assetFiles";
+
+    private static final String GET_ASSET = "SELECT * FROM assetFiles WHERE name=?";
+
+    private static final String DELETE_ASSET = "DELETE FROM assetFiles WHERE name=?";
+
+    private static final String COUNT_ROWS = "SELECT COUNT(*) FROM address";
+
+    private Connection connection;
+
+    private PreparedStatement addAsset;
+
+    // Not sure if this will be used yet delete(or not) once investigated#####################################
+    private PreparedStatement getNameList;
+
+    private PreparedStatement getAsset;
+
+    private PreparedStatement deleteAsset;
+
+    private PreparedStatement rowCount;
+
+    public ImageAssetDataSource() {
+        connection = DBConnection.getInstance();
+        try {
+            Statement st = connection.createStatement();
+            st.execute(CREATE_TABLE);
+            addAsset = connection.prepareStatement(INSERT_ASSET);
+            getNameList = connection.prepareStatement(GET_NAMES);
+            getAsset = connection.prepareStatement(GET_ASSET);
+            deleteAsset = connection.prepareStatement(DELETE_ASSET);
+            rowCount = connection.prepareStatement(COUNT_ROWS);
+        } catch (SQLException ex) {
+            // Improve this behavior ########################################################
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+
+     */
+    public void addImageFile(ImageAssetFile p) {
+        try {
+
+            addAsset.setString(1, p.getName());
+// Delete once the bufferedImageToBlob() method has been tested.###################################
+//            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//            ImageIO.write(p.getImageFile(), "jpg", baos);
+//            byte[] bytes = baos.toByteArray();
 //
-//    public static final String CREATE_TABLE =
-//            "CREATE TABLE IF NOT EXISTS assetFiles ("
-//                    + "idx INTEGER PRIMARY KEY /*!40101 AUTO_INCREMENT */ NOT NULL UNIQUE," // from https://stackoverflow.com/a/41028314
-//                    + "name VARCHAR(30) NOT NULL UNIQUE,"
-//                    + "imageFile BLOB(MAX),";
 //
-//    private static final String INSERT_ASSET = "INSERT INTO assetFiles (name, imageFile) VALUES (?, ?, ?, ?, ?);";
-//
-//    private static final String GET_NAMES = "SELECT name FROM assetFiles";
-//
-//    private static final String GET_ASSET = "SELECT * FROM assetFiles WHERE name=?";
-//
-//    private static final String DELETE_ASSET = "DELETE FROM assetFiles WHERE name=?";
-//
-//    private static final String COUNT_ROWS = "SELECT COUNT(*) FROM address";
-//
-//    private Connection connection;
-//
-//    private PreparedStatement addAsset;
-//
-//    // Not sure if this will be used yet delete(or not) once investigated#####################################
-//    private PreparedStatement getNameList;
-//
-//    private PreparedStatement getAsset;
-//
-//    private PreparedStatement deleteAsset;
-//
-//    private PreparedStatement rowCount;
-//
-//    public ImageAssetDataSource() {
-//        connection = DBConnection.getInstance();
-//        try {
-//            Statement st = connection.createStatement();
-//            st.execute(CREATE_TABLE);
-//            addAsset = connection.prepareStatement(INSERT_ASSET);
-//            getNameList = connection.prepareStatement(GET_NAMES);
-//            getAsset = connection.prepareStatement(GET_ASSET);
-//            deleteAsset = connection.prepareStatement(DELETE_ASSET);
-//            rowCount = connection.prepareStatement(COUNT_ROWS);
-//        } catch (SQLException ex) {
-//            // Improve this behavior ########################################################
-//            ex.printStackTrace();
-//        }
-//    }
-//
-//    /**
-//
-//     */
-//    public void addImageFile(ImageAssetFile p) {
-//        try {
-//
-//            addAsset.setString(1, p.getName());
-//// Delete once the bufferedImageToBlob() method has been tested.###################################
-////            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-////            ImageIO.write(p.getImageFile(), "jpg", baos);
-////            byte[] bytes = baos.toByteArray();
-////
-////
-////            Blob blob = new SerialBlob( bytes );
-//
-//            // this puts the data into the query
-//            addAsset.setBlob(2, bufferedImageToBlob(p.getImageFile(), "jpg"));
-//            addAsset.execute();
-//        } catch (SQLException ex) {
-//            ex.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace(); // Improve this behavior###########################################
-//        }
-//    }
-//
-//    /**
-//     * @see dataExercisenew.AddressBookDataSource#nameSet()
-//     * Probably don't need this one I previously deleted it from the interface. Remove once confirmed########################
-//     */
-////    public Set<String> setImageFile() {
-////        Set<String> names = new TreeSet<String>();
-////        ResultSet rs = null;
-////
-////        try {
-////            rs = getNameList.executeQuery();
-////            while (rs.next()) {
-////                names.add(rs.getString("name"));
-////            }
-////        } catch (SQLException ex) {
-////            ex.printStackTrace();
-////        }
-////
-////        return names;
-////    }
-//
-//    /**
-//     * @see dataExercise.AddressBookDataSource#getPerson(java.lang.String)
-//     * Continue from here ###### Continue from here ###### Continue from here ###### Continue from here ######
-//     *  Continue from here ###### Continue from here ###### Continue from here ###### Continue from here ######
-//     *   Continue from here ###### Continue from here ###### Continue from here ###### Continue from here ######
-//     */
-//    public ImageAssetFile getImageFile(String name) {
-//
-//        // I prepared this method for converting a Blob
-////        blobToBufferedImage();
-//
-//        ImageAssetFile p = new ImageAssetFile();
-//        ResultSet rs = null;
-//        /* BEGIN MISSING CODE */
-////        try {
-////            getPerson.setString(1, name);
-////            rs = getPerson.executeQuery();
-////            rs.next();
-////            p.setName(rs.getString("name"));
-////            p.setStreet(rs.getString("street"));
-////            p.setSuburb(rs.getString("suburb"));
-////            p.setPhone(rs.getString("phone"));
-////            p.setEmail(rs.getString("email"));
-////        } catch (SQLException ex) {
-////            ex.printStackTrace();
-////        }
-//        /* END MISSING CODE */
-//        return p;
-//    }
-//
-//    /**
-//     * @see dataExercise.AddressBookDataSource#getSize()
-//     */
-//    public int getSize() {
-//        ResultSet rs = null;
-//        int rows = 0;
-//
-//        /* BEGIN MISSING CODE */
-//        try {
-//            rs = rowCount.executeQuery();
-//            rs.next();
-//            rows = rs.getInt(1);
-//        } catch (SQLException ex) {
-//            ex.printStackTrace();
-//        }
-//        /* END MISSING CODE */
-//
-//        return rows;
-//    }
-//
-//    /**
-//     * @see dataExercise.AddressBookDataSource#deletePerson(java.lang.String)
-//     */
-//    public void deletePerson(String name) {
-//        /* BEGIN MISSING CODE */
-////        try {
-////            deletePerson.setString(1, name);
-////            deletePerson.executeUpdate();
-////        } catch (SQLException ex) {
-////            ex.printStackTrace();
-////        }
-//        /* END MISSING CODE */
-//    }
-//
-//    /**
-//     * @see dataExercise.AddressBookDataSource#close()
-//     */
-//    public void close() {
-//        /* BEGIN MISSING CODE */
-//        try {
-//            connection.close();
-//        } catch (SQLException ex) {
-//            ex.printStackTrace();
-//        }
-//        /* END MISSING CODE */
-//    }
-//}
+//            Blob blob = new SerialBlob( bytes );
+
+            // this puts the data into the query
+            addAsset.setBlob(2, bufferedImageToBlob(p.getImageFile(), "jpg"));
+            addAsset.execute();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace(); // Improve this behavior###########################################
+        }
+    }
+
+    /**
+     *
+     */
+    public Set<String> setImageFile() {
+        Set<String> names = new TreeSet<String>();
+        ResultSet rs = null;
+
+        try {
+            rs = getNameList.executeQuery();
+            while (rs.next()) {
+                names.add(rs.getString("name"));
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return names;
+    }
+
+    /**
+     *
+     */
+    public ImageAssetFile getImageFile(String name) {
+
+        ImageAssetFile a = new ImageAssetFile();
+        ResultSet rs = null;
+        try {
+            getAsset.setString(1, name);
+            rs = getAsset.executeQuery();
+            rs.next();
+            a.setName(rs.getString("name"));
+            a.setImageFile(blobToBufferedImage(rs.getBlob("name")));
+        } catch (SQLException ex) {
+            ex.printStackTrace(); // Improve error handling behavior ############################################
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return a;
+    }
+
+    /**
+     * Copied and unchanged from the week 6 tutorial.
+     */
+    public int getSize() {
+        ResultSet rs = null;
+        int rows = 0;
+
+        try {
+            rs = rowCount.executeQuery();
+            rs.next();
+            rows = rs.getInt(1);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return rows;
+    }
+
+    /**
+     * Unchanged from the week 6 tutorial.
+     */
+    public void deleteAsset(String name) {
+        try {
+            deleteAsset.setString(1, name);
+            deleteAsset.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Closes the database connection. Code copied from week 6 tutorial.
+     */
+    public void close() {
+        try {
+            connection.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
